2fef5bfe97bb5f1081c9fc0dd115b5684dce0718
Oleg Savchuk <osalabs+github@gmail.com>
2021-08-20 07:54:59 -0500

    SiteUtils.cs

---
+++ www/App_Code/SiteUtils.cs
@@ -0,0 +1,16 @@
+// Custom Site Utils
+// put misc utils for your web application here
+//
+// Part of ASP.NET osa framework  www.osalabs.com/osafw/asp.net-core
+// (c) 2009-2021 Oleg Savchuk www.osalabs.com
+
+namespace osafw
+{
+    public class SiteUtils
+    {
+        public static string sample()
+        {
+            return "";
+        }
+    }
+}
