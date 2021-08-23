ec8559e1caf63790195b97050eed0901f0defd4f
Kramarenko Nikita <nikita.kramarenko@engineeredit.com>
2021-08-23 18:49:58 +0300

    more DB unit tests

--- TestsProject/App_Code/fw/DBTests.cs
+++ TestsProject/App_Code/fw/DBTests.cs
@@ -24,6 +24,7 @@ namespace osafw.Tests
             db = new DB(connstr, "SQL", "main");
             db.connect();
             // create tables for testing
+            db.exec("DROP TABLE IF EXISTS " + table_name + ";");
             db.exec("CREATE TABLE " + table_name + " (  id INT,iname NVARCHAR(64) NOT NULL default '')");
 
             db.exec("INSERT INTO " + table_name + "(id, iname) VALUES(1,'test1'),(2,'test2'),(3,'test3');");
@@ -110,6 +111,7 @@ namespace osafw.Tests
             Assert.IsTrue(row.ContainsKey("id"));
             Assert.IsTrue(row.ContainsKey("iname"));
             Assert.AreEqual("test1", row["iname"]);
+            // TODO test all methods types
         }
 
         [TestMethod()]
@@ -126,6 +128,7 @@ namespace osafw.Tests
             Assert.AreEqual("test1", rows[0]["iname"]);
             Assert.AreEqual("test2", rows[1]["iname"]);
             Assert.AreEqual("test3", rows[2]["iname"]);
+            // TODO test all methods types
         }
 
         [TestMethod()]
@@ -136,6 +139,7 @@ namespace osafw.Tests
             Assert.AreEqual("test1", col[0]);
             Assert.AreEqual("test2", col[1]);
             Assert.AreEqual("test3", col[2]);
+            // TODO test all methods types
         }
 
         [TestMethod()]
@@ -143,6 +147,7 @@ namespace osafw.Tests
         {
             string value = (string)db.value("SELECT iname FROM " + table_name + " WHERE id=1;");
             Assert.AreEqual("test1", value);
+            // TODO test all methods types
         }
 
         [TestMethod()]
@@ -166,49 +171,69 @@ namespace osafw.Tests
         [TestMethod()]
         public void q_identTest()
         {
-            throw new NotImplementedException();
+            string r = db.q_ident(table_name);
+            Assert.AreEqual("[" + table_name + "]", r);
         }
 
         [TestMethod()]
         public void qTest()
         {
-            throw new NotImplementedException();
-        }
+            string r = db.q(table_name);
+            Assert.AreEqual("'" + table_name + "'", r);
 
-        [TestMethod()]
-        public void qTest1()
-        {
-            throw new NotImplementedException();
+            r = db.q("test'test");
+            Assert.AreEqual("'test''test'", r);
         }
+        
 
         [TestMethod()]
         public void qqTest()
         {
-            throw new NotImplementedException();
+            string r = db.qq("test'test");
+            Assert.AreEqual("test''test", r);
         }
 
         [TestMethod()]
         public void qiTest()
         {
-            throw new NotImplementedException();
+            int r = db.qi(123);
+            Assert.AreEqual(123, r);
+            r = db.qi("123");
+            Assert.AreEqual(123, r);
+            r = db.qi("AAA123");
+            Assert.AreEqual(0, r);
         }
 
         [TestMethod()]
         public void qfTest()
         {
-            throw new NotImplementedException();
+            double r = db.qf(123.123);
+            Assert.AreEqual(123.123, r);
+            r = db.qf("123.123");
+            Assert.AreEqual(123.123, r);
+            r = db.qf("AAA123.123");
+            Assert.AreEqual(0, r);
         }
 
         [TestMethod()]
         public void qdTest()
         {
-            throw new NotImplementedException();
+            string s = "2021-08-08 01:02:03";
+            string r = db.qd(s);
+            Assert.AreEqual("convert(DATETIME2, '" + s + "', 120)", r);
+
+            r = db.qd("");
+            Assert.AreEqual("NULL", r);
+
+            // TODO test with non SQL connnection
         }
 
         [TestMethod()]
         public void quoteTest()
         {
-            throw new NotImplementedException();
+            Hashtable r = db.quote(table_name, DB.h("id", "5", "iname", "test5"));
+            Assert.AreEqual("5", r["[id]"]);
+            Assert.AreEqual("'test5'", r["[iname]"]);
         }
 
         [TestMethod()]
@@ -226,103 +251,163 @@ namespace osafw.Tests
         [TestMethod()]
         public void opNOTTest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opNOT("test_value");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.NOT, r.op);
+            Assert.AreEqual("<>", r.opstr);
+            Assert.AreEqual("test_value", r.value);
         }
 
         [TestMethod()]
         public void opLETest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opLE("test_value");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.LE, r.op);
+            Assert.AreEqual("<=", r.opstr);
+            Assert.AreEqual("test_value", r.value);
         }
 
         [TestMethod()]
         public void opLTTest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opLT("test_value");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.LT, r.op);
+            Assert.AreEqual("<", r.opstr);
+            Assert.AreEqual("test_value", r.value);
         }
 
         [TestMethod()]
         public void opGETest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opGE("test_value");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.GE, r.op);
+            Assert.AreEqual(">=", r.opstr);
+            Assert.AreEqual("test_value", r.value);
         }
 
         [TestMethod()]
         public void opGTTest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opGT("test_value");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.GT, r.op);
+            Assert.AreEqual(">", r.opstr);
+            Assert.AreEqual("test_value", r.value);
         }
 
         [TestMethod()]
         public void opISNULLTest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opISNULL();
+            Assert.IsFalse(r.is_value);
+            Assert.AreEqual(DBOps.ISNULL, r.op);
+            Assert.AreEqual("IS NULL", r.opstr);
+            Assert.IsNull(r.value);
         }
 
         [TestMethod()]
         public void opISNOTNULLTest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opISNOTNULL();
+            Assert.IsFalse(r.is_value);
+            Assert.AreEqual(DBOps.ISNOTNULL, r.op);
+            Assert.AreEqual("IS NOT NULL", r.opstr);
+            Assert.IsNull(r.value);
         }
 
         [TestMethod()]
         public void opLIKETest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opLIKE("test_value");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.LIKE, r.op);
+            Assert.AreEqual("LIKE", r.opstr);
+            Assert.AreEqual("test_value", r.value);
         }
 
         [TestMethod()]
         public void opNOTLIKETest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opNOTLIKE("test_value");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.NOTLIKE, r.op);
+            Assert.AreEqual("NOT LIKE", r.opstr);
+            Assert.AreEqual("test_value", r.value);
         }
 
         [TestMethod()]
         public void opINTest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opIN("test_value", "test_value2");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.IN, r.op);
+            Assert.AreEqual("IN", r.opstr);
+            Assert.AreEqual("test_value", (string)((object[])r.value)[0]);
+            Assert.AreEqual("test_value2", (string)((object[])r.value)[1]);
         }
 
         [TestMethod()]
         public void opNOTINTest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opNOTIN("test_value", "test_value2");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.NOTIN, r.op);
+            Assert.AreEqual("NOT IN", r.opstr);
+            Assert.AreEqual("test_value", (string)((object[])r.value)[0]);
+            Assert.AreEqual("test_value2", (string)((object[])r.value)[1]);
         }
 
         [TestMethod()]
         public void opBETWEENTest()
         {
-            throw new NotImplementedException();
+            DBOperation r = db.opBETWEEN("test_value", "test_value2");
+            Assert.IsTrue(r.is_value);
+            Assert.AreEqual(DBOps.BETWEEN, r.op);
+            Assert.IsNull(r.opstr);
+            Assert.AreEqual("test_value", (string)((object[])r.value)[0]);
+            Assert.AreEqual("test_value2", (string)((object[])r.value)[1]);
         }
 
         [TestMethod()]
         public void insertTest()
         {
-            throw new NotImplementedException();
+            db.insert(table_name, new DBRow(DB.h("id", 5, "iname", "test5")));
+            DBRow r = db.row(table_name, DB.h("id", 5));
+            Assert.AreEqual("test5", r["iname"]);
+            // TODO test all methods types
         }
 
         [TestMethod()]
         public void updateTest()
         {
-            throw new NotImplementedException();
-        }
+            db.update(table_name, new DBRow(DB.h("iname", "test5")), DB.h("id", 3));
+            DBRow r = db.row(table_name, DB.h("id", 3));
+            Assert.AreEqual("test5", r["iname"]);
 
-        [TestMethod()]
-        public void updateTest1()
-        {
-            throw new NotImplementedException();
+            // TODO test all methods types
         }
 
         [TestMethod()]
         public void update_or_insertTest()
         {
-            throw new NotImplementedException();
+            db.update_or_insert(table_name, new DBRow(DB.h("iname", "test5")), DB.h("id", 5));
+            DBRow r = db.row(table_name, DB.h("id", 5));
+            Assert.AreEqual("test5", r["iname"]);
+
+            db.update_or_insert(table_name, new DBRow(DB.h("iname", "test5")), DB.h("id", 3));
+            r = db.row(table_name, DB.h("id", 3));
+            Assert.AreEqual("test5", r["iname"]);
         }
 
         [TestMethod()]
         public void delTest()
         {
-            throw new NotImplementedException();
+            db.del(table_name, DB.h("id", 3));
+            DBRow r = db.row(table_name, DB.h("id", 3));
+            Assert.IsTrue(r.Count == 0);
         }
 
         [TestMethod()]
@@ -369,12 +454,6 @@ namespace osafw.Tests
 
         [TestMethod()]
         public void clear_schema_cacheTest()
-        {
-            throw new NotImplementedException();
-        }
-
-        [TestMethod()]
-        public void DisposeTest()
         {
             throw new NotImplementedException();
         }
