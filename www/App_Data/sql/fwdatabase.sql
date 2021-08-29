84da2c00412ab2a396f3c17de1c4262f0469f6d2
Kramarenko Nikita <nikita.kramarenko@engineeredit.com>
2021-08-29 11:27:43 +0300

    Add user_filrers table to fwdatabase.sql

--- www/App_Data/sql/fwdatabase.sql
+++ www/App_Data/sql/fwdatabase.sql
@@ -354,3 +354,19 @@ CREATE TABLE menu_items (
   upd_users_id          INT DEFAULT 0
 );
 -- INSERT INTO menu_items (iname, url, icon, controller) VALUES ('Test Menu Item', '/Admin/Demos', 'list-ul', 'AdminDemos');
+DROP TABLE IF EXISTS user_filters;
+CREATE TABLE user_filters (
+  id                    INT IDENTITY(1,1) PRIMARY KEY CLUSTERED,
+  icode                 NVARCHAR(128) NOT NULL, -- related screen, ex: GLOBAL[controller.action]
+
+  iname                 NVARCHAR(255) NOT NULL,
+  idesc                 NVARCHAR(MAX), -- json with filter data
+  is_system             TINYINT NOT NULL DEFAULT 0, -- 1 - system - visible for all
+  is_shared             TINYINT NOT NULL DEFAULT 0, -- 1 if shared/published
+
+  status                TINYINT DEFAULT 0,
+  add_time              DATETIME2 NOT NULL DEFAULT getdate(),
+  add_users_id          INT DEFAULT 0, -- related owner user
+  upd_time              DATETIME2,
+  upd_users_id          INT DEFAULT 0
+);
