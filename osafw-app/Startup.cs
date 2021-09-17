f0b4394539dac7ef23c8bf4321f1a136e01021f9
Kramarenko Nikita <nikita.kramarenko@engineeredit.com>
2021-09-17 18:44:36 +0300

    serve files in upload dir as static files

--- osafw-app/Startup.cs
+++ osafw-app/Startup.cs
@@ -57,6 +57,11 @@ namespace osafw
                 app.UseHttpsRedirection();
             }
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"upload")),
+                RequestPath = new PathString("/upload")
+            });
             app.UseSession();
             // TODO MIGRATE app.UseCookiePolicy(); if not use standard auth identity
             // TODO MIGRATE reponse cacheing https://docs.microsoft.com/en-us/aspnet/core/performance/caching/middleware?view=aspnetcore-5.0
