a23899c79474ddc3b9a98778ff2d5132efc9235e
Oleg Savchuk <osalabs+github@gmail.com>
2022-02-09 16:05:14 -0600

    removed duplicate att_categories sql

--- osafw-app/App_Data/sql/fwdatabase.sql
+++ osafw-app/App_Data/sql/fwdatabase.sql
@@ -10,25 +10,6 @@ CREATE TABLE fwsessions (
   AbsoluteExpiration    DATETIMEOFFSET NULL
 );
 
-
-/* upload categories */
-DROP TABLE IF EXISTS att_categories;
-CREATE TABLE att_categories (
-  id int IDENTITY(1,1) PRIMARY KEY CLUSTERED,
-
-  icode                 NVARCHAR(64) NOT NULL DEFAULT '', /*to use from code*/
-  iname                 NVARCHAR(64) NOT NULL DEFAULT '',
-  idesc                 NVARCHAR(MAX),
-  prio                  INT NOT NULL DEFAULT 0,     /* 0 is normal and lowest priority*/
-
-  status                TINYINT NOT NULL DEFAULT 0,        /*0-ok, 127-deleted*/
-  add_time              DATETIME2 NOT NULL DEFAULT getdate(),
-  add_users_id          INT DEFAULT 0,
-  upd_time              DATETIME2,
-  upd_users_id          INT DEFAULT 0
-);
-
-
 /* upload categories */
 DROP TABLE IF EXISTS att_categories;
 CREATE TABLE att_categories (
