86786a3292d641ec204cf34771414dc1b69ae533
Oleg Savchuk <osalabs+github@gmail.com>
2023-05-31 16:46:12 -0500

    blue theme wip

--- osafw-app/App_Data/template/layout/common.js
+++ osafw-app/App_Data/template/layout/common.js
@@ -2,17 +2,3 @@ $(document).on('click', '.on-sidebar-toggler', function (e) {
   $('body').toggleClass('no-sidebar');
   $('#sidebar').removeClass('show');
 });
-
-function getPrefUIMode() {
-  let mode = document.documentElement.getAttribute('data-ui-mode');
-  if (mode) return mode;
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-}
-function setUIMode(mode) {
-  if (mode === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.setAttribute('data-bs-theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-bs-theme', mode);
-  }
-}
-//setUIMode(getPrefUIMode());
