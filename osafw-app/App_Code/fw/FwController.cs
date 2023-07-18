57fef73ab90bb1719bb5851553bfe04b038c12e2
Vlad Savchuk <vlad.savchuk.ua@gmail.com>
2023-07-18 10:22:59 +0300

    preserve for all

--- osafw-app/App_Code/fw/FwController.cs
+++ osafw-app/App_Code/fw/FwController.cs
@@ -755,8 +755,6 @@ public abstract class FwController
                 // or return to add new form
                 url = this.base_url + "/new";
                 url_q += "&copy_id=" + id;
-                if (!string.IsNullOrEmpty(return_url))
-                    url_q += "&return_url=" + Utils.urlescape(return_url);
                 is_add_new = true;
             }
             else
@@ -765,6 +763,10 @@ public abstract class FwController
         }
         else
             url = this.base_url;
+
+        //preserve return url if present
+        if (!string.IsNullOrEmpty(return_url))
+            url_q += "&return_url=" + Utils.urlescape(return_url);
 
         //add base_url_suffix if any
         if (!string.IsNullOrEmpty(base_url_suffix))
