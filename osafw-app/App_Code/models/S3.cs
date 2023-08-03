77fec8af1410280b12538ad57ec0e2fe67d20319
Vlad Savchuk <vlad.savchuk.ua@gmail.com>
2023-08-03 20:48:00 +0300

    S3 Storage Class support

--- osafw-app/App_Code/models/S3.cs
+++ osafw-app/App_Code/models/S3.cs
@@ -105,29 +105,25 @@ public class S3 : FwModel
     /// <summary>
     /// upload local file by filepath to the S3
     /// </summary>
+    /// <remarks>
+    /// S3 Storage Classes: https://docs.aws.amazon.com/sdkfornet/v3/apidocs/items/S3/TS3StorageClass.html
+    /// </remarks>
     /// <param name="key">relative to the S3Root</param>
     /// <param name="file">file from http upload</param>
     /// <param name="disposition">if defined (ex: inline) - Content-Disposition with file.FileName added</param>
     /// <param name="filename">optional filename to include in disposition header</param>
-    /// <param name="storage_class">S3 Storage Class, default is STANDARD, can be GLACIER_IR for warm archive files</param>
+    /// <param name="storage_class">S3 Storage Class, default is Amazon.S3.S3StorageClass.Standard, use 5 times cheaper Amazon.S3.S3StorageClass.GlacierInstantRetrieval for warm archive files.</param>
     /// <returns></returns>
-    public PutObjectResponse uploadLocalFile(string key, string filepath, string disposition = "", string filename = "", string storage_class = "STANDARD")
+    public PutObjectResponse uploadLocalFile(string key, string filepath, string disposition = "", string filename = "", S3StorageClass storage_class = null)
     {
         logger("uploading to S3: key=[" + key + "], filepath=[" + filepath + "]");
-
-        // https://docs.aws.amazon.com/sdkfornet/v3/apidocs/items/S3/TS3StorageClass.html
-        S3StorageClass s3_storage_class;
-        if (storage_class == "GLACIER_IR")
-            s3_storage_class = S3StorageClass.GlacierInstantRetrieval;
-        else
-            s3_storage_class = S3StorageClass.Standard;
 
         var request = new PutObjectRequest()
         {
             BucketName = this.bucket,
             Key = this.root + key,
             FilePath = filepath,
-            StorageClass = s3_storage_class
+            StorageClass = storage_class ?? S3StorageClass.Standard
         };
         request.Headers["Content-Type"] = UploadUtils.mimeMapping(filepath);
 
