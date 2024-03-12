4ed5a4fb8322299f2ed2278bb349a032ae543908
Oleg Savchuk <osalabs+github@gmail.com>
2024-03-12 10:27:52 -0500

    pagination works

--- osafw-app/App_Data/template/admin/demosvue/index/store.js
+++ osafw-app/App_Data/template/admin/demosvue/index/store.js
@@ -4,33 +4,54 @@ const apiUsers = mande('/api/users');
 const useFwStore = defineStore('fw', {
   state: () => ({
     global: {}, //global config
-    field_id: 'id',
-    headers: [], //list headers
-    headers_search: [], //list of search values for headers filter
-    count: 0, //total list rows count
-    list_rows: [],
-    pager: [], // array of { pagenum:N, pagenum_show:N, is_cur_page:0|1, is_show_first:0|1, is_show_prev:0|1, is_show_next:0|1, pagenum_next:N}
-    f: [], //list filter
-    related_id: 0,
-    base_url: '',
+    base_url: '', // base url for the controller
     is_userlists: false,
     is_readonly: false,
-    return_url: ''
+
+    // list filter, can contain: {pagenum:N, pagesize:N, sortby:'', sortdir:'asc|desc'}
+    f: {
+        pagenum: 0,
+        pagesize: 25,
+        sortby: '',
+        sortdir: '',
+    }, 
+    related_id: 0, // related model id
+    return_url: '', // return url if controller called from other place expecting user's return
+    field_id: 'id', // model's id field name
+    headers: [], // list headers, array of {field_name:"", field_name_visible:"", is_sortable:true|false, is_checked:true, search_value:null|""}
+    headers_search: [], // list of search values for headers filter TODO REMOVE
+    count: 0, // total list rows count
+    list_rows: [],
+    pager: [] // array of { pagenum:N, pagenum_show:N, is_cur_page:0|1, is_show_first:0|1, is_show_prev:0|1, is_show_next:0|1, pagenum_next:N}
   }),
+
   getters: {
     doubleCount: (state) => state.count * 2, //sample getter
   },
+
   actions: {
-    //sample action
-    increment(state) {
-      this.count++;
+    // set one or multiple filter values and reload list
+    setFilters(filters) {
+        console.log('setFilters', filters);
+        //merge filters into state.f
+        this.$state.f = { ...this.$state.f, ...filters };
+        this.loadIndex();
     },
     // load data
     async loadIndex() {
       try {
         const apiIndex = mande(this.base_url);
 
-        const data = await apiIndex.get();
+        // build request query from state.f, each parameter name should be int form "f[name]"
+        let req = { dofilter: 1 };
+        Object.keys(this.f).forEach(key => {
+            req['f['+key+']'] = this.f[key];
+        });
+        // add related_id to request
+        if (this.related_id) req.related_id = this.related_id;
+        console.log('loadIndex req', req);
+
+        const data = await apiIndex.get('', { query: req });
         console.log('loadIndex data', data);
 
         //save to store each key from data if such key exists in store
@@ -46,7 +67,7 @@ const useFwStore = defineStore('fw', {
       }
     },
 
-    // sample async action
+    // sample async action TODO REMOVE
     async registerUser(login, password) {
       try {
         this.userData = await apiUsers.post({ login, password });
