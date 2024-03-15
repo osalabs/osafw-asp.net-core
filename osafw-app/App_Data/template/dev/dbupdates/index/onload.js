6ae5f8f9deb7a38da293152823cd39cf1cfdf297
Nikita Kramarenko <nikita.kramarenko@engineeredit.com>
2024-03-15 11:37:54 +0200

    OSAFWCORE-78 minor fixes

--- osafw-app/App_Data/template/dev/dbupdates/index/onload.js
+++ osafw-app/App_Data/template/dev/dbupdates/index/onload.js
@@ -8,16 +8,5 @@ $('.on-apply-update').on('click', function(e) {
 
 });
 
-function highlightSQL() {
-    $('.sql-code').each(function(i, el){
-       var sqlCode = $(this).text();
-       // Replace text with highlighted spans
-       sqlCode = sqlCode.replace(/\b(SELECT|FROM|WHERE|AND|OR|INSERT|INTO|VALUES|UPDATE|SET|DELETE|ALTER|TABLE|ADD|REMOVE)\b/gi, '<span class="sql-keyword">$&</span>');
-       sqlCode = sqlCode.replace(/'([^']+)'/g, '<span class="sql-string">$&</span>');
-       sqlCode = sqlCode.replace(/(--[^\n]*)/g, '<span class="sql-comment">$&</span>');
-       
-       $(this).html(sqlCode);
-    });
-}
-
+<~../highlight.js>
 highlightSQL();
