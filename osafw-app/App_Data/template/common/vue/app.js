feabd33f540a9fc27961044f34e814c26ca83a1a
Oleg Savchuk <osalabs+github@gmail.com>
2024-04-10 15:07:50 -0500

    moved list-edit-pane inside list-table, moved app.js and store.js into /common/vue

---
+++ osafw-app/App_Data/template/common/vue/app.js
@@ -0,0 +1,40 @@
+//load store.js first with const useFwStore
+
+const mainApp = {
+  //data: () => ({
+  //    counter: 0
+  //}),
+  computed:{
+  },
+  mounted() {
+    console.log('mainApp mounted');
+    const fwStore = useFwStore();
+    // assign all data from this.$el.parentElement.dataset to keys existing in fwStore
+      Object.keys(this.$el.parentElement.dataset).forEach(key => {
+          console.log("data key:", key,"=", this.$el.parentElement.dataset[key]);
+        if (fwStore[key] !== undefined) fwStore[key] = this.$el.parentElement.dataset[key];
+    });
+    fwStore.loadIndex();
+  },
+  updated() {
+    //console.log('mainApp updated');
+  },
+  methods:{
+    reload(){
+      window.location.reload();
+    },
+  }
+};
+
+const app = createApp(mainApp);
+window.fwApp = app; //make app available for components below in html
+
+const pinia = createPinia();
+app.use(pinia);
+
+// https://github.com/vueform/multiselect
+//app.component('multiselect', VueformMultiselect);
+
+//components - add load to vue_components
+
+//mounted to #fw-app in /layout/vue/sys_footer
