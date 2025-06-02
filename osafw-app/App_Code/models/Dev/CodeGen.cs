c95ced7a4fb0217830677d848c4fc5fbb96f98aa
Oleg Savchuk <osalabs+github@gmail.com>
2025-06-02 13:44:49 -0500

    Add quick report generation

--- osafw-app/App_Code/models/Dev/CodeGen.cs
+++ osafw-app/App_Code/models/Dev/CodeGen.cs
@@ -711,7 +711,8 @@ class DevCodeGen
 
         List<Hashtable> formTabs = [
             //default
-            new Hashtable {
+            new Hashtable
+            {
                 ["tab"] = "",
                 ["label"] = "Main"
             }
@@ -1430,6 +1431,35 @@ class DevCodeGen
                     }
             }
         }
+    }
+
+    public void createReport(string repcode)
+    {
+        repcode = FwReports.cleanupRepcode(repcode);
+        if (string.IsNullOrEmpty(repcode))
+            throw new UserException("No report code");
+
+        var report_class = FwReports.repcodeToClass(repcode);
+        var reports_path = fw.config("site_root") + @"\App_Code\models\Reports";
+        var src_file = reports_path + @"\Sample.cs";
+        var dest_file = reports_path + @"\" + report_class.Replace("Report", "") + ".cs";
+
+        if (File.Exists(dest_file))
+            throw new UserException("Such report already exists");
+
+        var content = Utils.getFileContent(src_file);
+        if (content == "")
+            throw new ApplicationException("Can't open Sample.cs");
+
+        content = content.Replace("SampleReport", report_class);
+        content = content.Replace("Sample report", Utils.capitalize(repcode) + " report");
+
+        Utils.setFileContent(dest_file, ref content);
+
+        // copy templates
+        var tpl_from = fw.config("template") + "/admin/reports/sample";
+        var tpl_to = fw.config("template") + "/admin/reports/" + repcode.ToLower();
+        Utils.CopyDirectory(tpl_from, tpl_to, true);
     }
     // update by url
     private void updateMenuItem(string controller_url, string controller_title)
