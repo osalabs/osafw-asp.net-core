caac9a3b419bed51a8cb38ba1f389edfc4f27141
Oleg Savchuk <osalabs+github@gmail.com>
2025-08-19 14:01:35 -0500

    sidebar flickering fix

--- osafw-app/App_Data/template/layout/common.js
+++ osafw-app/App_Data/template/layout/common.js
@@ -1,13 +1,1 @@
-// Sidebar toggler/state
-const sidebarCollapsed = sessionStorage.getItem('sidebar-collapsed') === 'true';
-if (sidebarCollapsed && window.matchMedia('(min-width: 768px)').matches) {
-  $('body').addClass('no-sidebar');
-}
-
-$(document).on('click', '.on-sidebar-toggler', function (e) {
-  // allow default behavior so Bootstrap collapse works
-  if (window.matchMedia('(min-width: 768px)').matches) {
-    $('body').toggleClass('no-sidebar');
-    sessionStorage.setItem('sidebar-collapsed', $('body').hasClass('no-sidebar'));
-  }
-});
+<~/layout/sidebar_toggler.js>
