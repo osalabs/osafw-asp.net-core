3c0c3ef8c7e3766b5a196941f481ccf6b7224f80
Oleg Savchuk <osalabs+github@gmail.com>
2025-11-12 16:16:02 -0600

    moved password reset to PasswordReset to avoid custom action in reset email link

--- osafw-app/App_Code/controllers/Password.cs
+++ osafw-app/App_Code/controllers/Password.cs
@@ -12,8 +12,6 @@ public class PasswordController : FwController
 {
     protected Users model = new();
 
-    protected int PWD_RESET_EXPIRATION = 60; // minutes
-
     public override void init(FW fw)
     {
         base.init(fw);
@@ -61,89 +59,6 @@ public class PasswordController : FwController
         model.sendPwdReset(user["id"].toInt());
 
         fw.redirect(base_url + "/(Sent)");
-    }
-
-    public Hashtable ResetAction()
-    {
-        Hashtable ps = [];
-        var login = reqs("login");
-        var token = reqs("token");
-        var user = model.oneByEmail(login);
-        if (user.Count == 0 || user["status"].toInt() != Users.STATUS_ACTIVE)
-            throw new UserException("Not a valid Email");
-
-        if (user["pwd_reset"] == "" || !model.checkPwd(token, user["pwd_reset"], Users.PWD_RESET_TOKEN_LEN)
-            || (db.Now() - DateTime.Parse(user["pwd_reset_time"])).TotalMinutes > PWD_RESET_EXPIRATION)
-        {
-            fw.flash("error", "Password reset token expired. Use Forgotten password link again.");
-            fw.redirect("/Login");
-        }
-
-        var item = reqh("item");
-        if (isGet())
-            // set defaults here
-            item = [];
-        else
-        {
-        }
-
-        ps["user"] = user;
-        ps["token"] = token;
-        ps["i"] = item;
-        ps["hide_sidebar"] = true;
-
-        return ps;
-    }
-
-
-    public void SaveResetAction()
-    {
-        route_onerror = "Reset"; //set route to go if error happens
-
-        var item = reqh("item");
-        var login = reqs("login");
-        var token = reqs("token");
-        var user = model.oneByEmail(login);
-        if (user.Count == 0 || user["status"].toInt() != Users.STATUS_ACTIVE)
-            throw new UserException("Not a valid Email");
-
-        if (user["pwd_reset"] == "" || !model.checkPwd(token, user["pwd_reset"], Users.PWD_RESET_TOKEN_LEN)
-            || (db.Now() - DateTime.Parse(user["pwd_reset_time"])).TotalMinutes > PWD_RESET_EXPIRATION)
-        {
-            fw.flash("error", "Password reset token expired. Use Forgotten password link again.");
-            fw.redirect("/Login");
-        }
-
-        int id = user["id"].toInt();
-
-        ValidateReset(id, item);
-        // load old record if necessary
-        // Dim itemdb As Hashtable = Users.one(id)
-
-        var itemdb = FormUtils.filter(item, Utils.qw("pwd"));
-
-        itemdb["pwd_reset"] = ""; // also reset token
-        model.update(id, itemdb);
-
-        fw.logActivity(FwLogTypes.ICODE_USERS_CHPWD, FwEntities.ICODE_USERS);
-        fw.flash("success", "Password updated");
-
-        fw.redirect("/Login");
-    }
-
-    public void ValidateReset(int id, Hashtable item)
-    {
-        bool result = true;
-        result &= validateRequired(id, item, Utils.qw("pwd pwd2"));
-        if (!result)
-            fw.FormErrors["REQ"] = 1;
-
-        if (result && (string)item["pwd"] != (string)item["pwd2"])
-        {
-            fw.FormErrors["pwd2"] = "NOTEQUAL";
-        }
-
-        this.validateCheckResult();
     }
 
     public Hashtable SentAction()
---
+++ osafw-app/App_Code/controllers/PasswordReset.cs
@@ -0,0 +1,109 @@
+// Password Reset controller
+//
+// Part of ASP.NET osa framework  www.osalabs.com/osafw/asp.net
+// (c) 2009-2025 Oleg Savchuk www.osalabs.com
+
+using System;
+using System.Collections;
+
+namespace osafw;
+
+public class PasswordResetController : FwController
+{
+    protected Users model = new();
+
+    protected int PWD_RESET_EXPIRATION = 60; // minutes
+
+    public override void init(FW fw)
+    {
+        base.init(fw);
+        model.init(fw);
+        base_url = "/PasswordReset"; // base url for the controller
+        // override layout
+        fw.G["PAGE_LAYOUT"] = fw.G["PAGE_LAYOUT_PUBLIC"];
+    }
+
+    public override void checkAccess()
+    {
+        //true - allow access to all, including visitors
+    }
+
+    // GET /PasswordReset?login=...&token=...
+    public Hashtable IndexAction()
+    {
+        Hashtable ps = [];
+        var login = reqs("login");
+        var token = reqs("token");
+        var user = model.oneByEmail(login);
+        if (user.Count == 0 || user["status"].toInt() != 0)
+            throw new UserException("Not a valid Email");
+
+        if ((string)user["pwd_reset"] == "" || !model.checkPwd(token, (string)user["pwd_reset"], Users.PWD_RESET_TOKEN_LEN)
+            || (db.Now() - DateTime.Parse((string)user["pwd_reset_time"])).TotalMinutes > PWD_RESET_EXPIRATION)
+        {
+            fw.flash("error", "Password reset token expired. Use Forgotten password link again.");
+            fw.redirect("/Login");
+        }
+
+        var item = reqh("item");
+        if (isGet())
+            item = [];
+
+        ps["user"] = user;
+        ps["token"] = token;
+        ps["i"] = item;
+        ps["hide_sidebar"] = true;
+
+        return ps;
+    }
+
+    // POST /PasswordReset
+    public void SaveAction()
+    {
+        route_onerror = FW.ACTION_INDEX; //set route to go if error happens
+
+        var item = reqh("item");
+        var login = reqs("login");
+        var token = reqs("token");
+        var user = model.oneByEmail(login);
+        if (user.Count == 0 || user["status"].toInt() != 0)
+            throw new UserException("Not a valid Email");
+
+        if ((string)user["pwd_reset"] == "" || !model.checkPwd(token, (string)user["pwd_reset"], Users.PWD_RESET_TOKEN_LEN)
+            || (db.Now() - DateTime.Parse((string)user["pwd_reset_time"])).TotalMinutes > PWD_RESET_EXPIRATION)
+        {
+            fw.flash("error", "Password reset token expired. Use Forgotten password link again.");
+            fw.redirect("/Login");
+        }
+
+        int id = user["id"].toInt();
+
+        Validate(id, item);
+
+        var itemdb = FormUtils.filter(item, Utils.qw("pwd"));
+
+        itemdb["pwd_reset"] = ""; // also reset token
+        model.update(id, itemdb);
+
+        fw.logActivity(FwLogTypes.ICODE_USERS_CHPWD, FwEntities.ICODE_USERS);
+        fw.flash("success", "Password updated");
+
+        fw.redirect("/Login");
+    }
+
+    public void Validate(int id, Hashtable item)
+    {
+        bool result = true;
+        result &= validateRequired(id, item, Utils.qw("pwd pwd2"));
+        if (!result)
+            fw.FormErrors["REQ"] = 1;
+
+        if (result && (string)item["pwd"] != (string)item["pwd2"])
+        {
+            result = false;
+            fw.FormErrors["pwd2"] = "NOTEQUAL";
+        }
+
+        this.validateCheckResult();
+    }
+}
