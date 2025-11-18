a46155dc9b413a88c81780ac88c0a872e8ed1ffe
Oleg Savchuk <osalabs+github@gmail.com>
2025-11-18 11:31:22 -0600

    Add configurable DB timezone and fix Vue datetime display

--- osafw-app/App_Code/fw/DateUtils.cs
+++ osafw-app/App_Code/fw/DateUtils.cs
@@ -10,7 +10,22 @@ namespace osafw;
 
 public class DateUtils
 {
-    public const string DATABASE_TZ = "Central Standard Time"; // timezone of the database server, default "UTC"
+    public const string DEFAULT_DATABASE_TZ = "Central Standard Time"; // timezone of the database server, default "UTC"
+    public static string DATABASE_TZ
+    {
+        get
+        {
+            var settings = FwConfig.settings;
+            if (settings != null)
+            {
+                var tz = settings["timezone_db"]?.ToString();
+                if (!string.IsNullOrEmpty(tz))
+                    return tz;
+            }
+
+            return DEFAULT_DATABASE_TZ;
+        }
+    }
 
     // keep in sync with template/common/sel/date_format.sel
     public const int DATE_FORMAT_MDY = 0; // M/D/YYYY
