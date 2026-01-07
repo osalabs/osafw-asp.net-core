5f49f565cc03eb8d712bcee4ba92977fc1e15173
Oleg Savchuk <osalabs+github@gmail.com>
2026-01-06 19:20:29 -0600

    fw.js class fix

--- osafw-app/wwwroot/assets/js/fw.js
+++ osafw-app/wwwroot/assets/js/fw.js
@@ -7,7 +7,7 @@
 window.fw={
   HTML_LOADING: '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...',
   HTML_SPINNER_CT: '<span class="fw-spinner-container"> <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span></span',
-  HTML_SPINNER_SM: '<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>',
+  HTML_SPINNER_SM: '<span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>',
   ICON_SORT_ASC: '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/></svg>',
   ICON_SORT_DESC: '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/></svg>',
   PWD_PROGRESS_BAR: '<div class="progress mt-1"><div class="progress-bar" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div></div>',
